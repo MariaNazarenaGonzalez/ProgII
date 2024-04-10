3f32766ec03568f7a77bac2011d625944548b2bf
MariaNazarenaGonzalez <101027506+MariaNazarenaGonzalez@users.noreply.github.com>
2024-04-10 15:31:58 -0300

    Update lista_cursor.c

--- TP2/lista_cursor.c
+++ TP2/lista_cursor.c
@@ -2,46 +2,58 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include "tipo_elemento.h"
 #include "lista.h"
 static const int TAMANIO_MAXIMO = 100;
 static const int NULO=-1;
+
 struct Nodo {
-    TipoElemento Dato;
-    int sig;
+    TipoElemento datos;
+    int siguiente;
 };
 
-struct ListaRep{
-    struct Nodo * cursor;
+struct ListaRep {
+    struct Nodo *cursor;
     int inicio;
     int libre;
     int cantidad;
-};
-typedef struct ListaRep *Lista;
-struct IteradorRep{
+};typedef struct ListaRep *Lista;
+
+struct IteradorRep {
     Lista lista;
-    int pos_actual;
-};
-typedef struct IteradorRep *Iterador;
+    int posicionActual;
+};typedef struct IteradorRep *Iterador;
+
 
 // Operaciones del TAD
 
 Lista l_crear (){
-    Lista l=(Lista)malloc(sizeof(struct ListaRep *));
-    l->cursor=calloc(TAMANIO_MAXIMO,sizeof(struct Nodo));
-    l->cantidad=0;
-    l->inicio=NULO;
-    for(int i=0;i<TAMANIO_MAXIMO-2;i++){
-        l->cursor[i].sig=i+1;
+    int i = 0;
+    Lista nueva_lista = (Lista) malloc(sizeof(struct ListaRep));
+
+    // TODO hacer flexible y que la lista crezca sola
+    nueva_lista->cursor = calloc(TAMANIO_MAXIMO, sizeof(struct Nodo));
+    nueva_lista->cantidad = 0;
+    nueva_lista->inicio = NULO;
+
+    // Encadeno todos los libres
+    for (i=0; i<=(TAMANIO_MAXIMO-2); i++) {
+        nueva_lista->cursor[i].siguiente = i+1;
     }
-    l->libre=0;
-    l->cursor[TAMANIO_MAXIMO-1].sig=NULO;
-    return l;
+
+    // Instancio inicio, libre y demas
+    nueva_lista->libre = 0;
+    nueva_lista->cursor[TAMANIO_MAXIMO-1].siguiente = NULO;
+    nueva_lista->inicio = NULO;
+
+    // retorno la lista creada
+    return nueva_lista;
 }
 //Crea la lista vacía. Retorna el puntero a la misma.
 
 bool l_es_vacia (Lista lista){
-    return (lista->libre==NULO);
+    return lista->cantidad == 0;
 }
 // determinar si la lista esta vacía. Retorna “true” cuando esto sucede, caso contrario retorna “False”.
 
@@ -55,21 +67,35 @@ int l_longitud (Lista lista){
 }
 //determinar la cantidad de elementos de la lista.
 
-bool l_agregar (Lista lista, TipoElemento elemento){
-    if(l_es_llena(lista)){return false;}
+bool l_agregar(Lista lista, TipoElemento elemento){
     int p;
-    p=lista->libre;
-    lista->libre=lista->cursor[p].sig;
-    lista->cursor[p].Dato=elemento;
-    lista->cursor[p].sig=NULO;
-    if(lista->inicio==NULO){
-        lista->inicio=p;
-    }else{
-        int q=lista->inicio;
-        while(lista->cursor[q].sig==NULO){
-            q=lista->cursor[q].sig;
+    int q;
+    int anteq;
+
+    // controlo lista llena
+    if (l_es_llena(lista)) {
+        return false;
+    }
+
+    // Tomo el primer libre
+    p = lista->libre;
+    lista->libre = lista->cursor[p].siguiente;
+
+    // Asigno el dato
+    lista->cursor[p].datos = elemento;
+    lista->cursor[p].siguiente = NULO;
+
+    // Controlo que no sea el primero de la lista
+    if (l_es_vacia(lista)) {
+        lista->inicio = p;
+    } else {
+        // lo ubico al final
+        q = lista->inicio;
+        while (q != NULO) {
+            anteq = q;  //guardo el anterior porque no tengo puntero al anterior
+            q = lista->cursor[q].siguiente;
         }
-        lista->cursor[q].sig=p;
+        lista->cursor[anteq].siguiente = p;
     }
     lista->cantidad++;
     return true;
@@ -77,17 +103,20 @@ bool l_agregar (Lista lista, TipoElemento elemento){
 //Agrega un elemento al final de la lista, incrementando la cantidad de elementos de la misma.
 
 bool l_borrar (Lista lista, int clave){
-    if (l_es_vacia(lista)) {return false;}
+    if (l_es_vacia(lista)) {
+        return false;
+    }
 
-    bool borre = false;int q;
+    bool borre = false;
+    int q;
     int p = lista->inicio;
 
     // borro las claves que coinciden con el inicio
-    while ((p != NULO) && (lista->cursor[p].Dato->clave == clave)) {
+    while ((p != NULO) && (lista->cursor[p].datos->clave == clave)) {
         q = p;
-        lista->inicio = lista->cursor[p].sig;
+        lista->inicio = lista->cursor[p].siguiente;
         // recupero el nodo en el libre para no perderlo
-        lista->cursor[q].sig = lista->libre;
+        lista->cursor[q].siguiente = lista->libre;
         lista->libre = q;
         // Descuento 1 y arranco de nuevo desde el inicio
         lista->cantidad--;
@@ -100,18 +129,18 @@ bool l_borrar (Lista lista, int clave){
     p = lista->inicio;
     while (p != NULO) {
         // pregunto por uno adelantado
-        if (lista->cursor[p].Dato->clave == clave) {
+        if (lista->cursor[p].datos->clave == clave) {
             q = p;
-            lista->cursor[qant].sig = lista->cursor[p].sig;
+            lista->cursor[qant].siguiente = lista->cursor[p].siguiente;
             // Preservo en el libre
-            lista->cursor[q].sig = lista->libre;
+            lista->cursor[q].siguiente = lista->libre;
             lista->libre = q;
             lista->cantidad--;
             p = qant;  //vuelvo a tomar el qant para revisar que no existan otras claves iguales
             borre = true;
         } else {
             qant = p;  // guardo el anterior
-            p = lista->cursor[p].sig;
+            p = lista->cursor[p].siguiente;
         }
     }
     return borre;
@@ -124,10 +153,10 @@ ocurrencias.*/
 TipoElemento l_buscar (Lista lista, int clave){
     int p = lista->inicio;
     while (p != NULO) {
-        if (lista->cursor[p].Dato->clave == clave) {
-            return lista->cursor[p].Dato;
+        if (lista->cursor[p].datos->clave == clave) {
+            return lista->cursor[p].datos;
         }
-        p = lista->cursor[p].sig;
+        p = lista->cursor[p].siguiente;
     }
     return NULL;
 }
@@ -145,21 +174,21 @@ bool l_insertar (Lista lista, TipoElemento elemento, int pos){
 
     // Sino asigna espacio tomando del libre
     int p = lista->libre;
-    lista->libre = lista->cursor[p].sig;
-    lista->cursor[p].Dato = elemento;
-    lista->cursor[p].sig = NULO;
+    lista->libre = lista->cursor[p].siguiente;
+    lista->cursor[p].datos = elemento;
+    lista->cursor[p].siguiente = NULO;
 
     // valida si es la primer posicion
     if (pos == 1) {
-        lista->cursor[p].sig= lista->inicio;
+        lista->cursor[p].siguiente = lista->inicio;
         lista->inicio = p;
     } else {
         int temp2 = lista->inicio;
         for (int i = 0; i < pos - 2; i++) {
-            temp2 = lista->cursor[temp2].sig;
+            temp2 = lista->cursor[temp2].siguiente;
         }
-        lista->cursor[p].sig = lista->cursor[temp2].sig;
-        lista->cursor[temp2].sig = p;
+        lista->cursor[p].siguiente = lista->cursor[temp2].siguiente;
+        lista->cursor[temp2].siguiente = p;
     }
     // Cuenta uno mas
     lista->cantidad++;
@@ -175,18 +204,18 @@ bool l_eliminar (Lista lista, int pos){
     if (1 <= pos && pos <= l_longitud(lista)) {
         if (pos == 1) {
             p = actual;
-            lista->inicio = lista->cursor[actual].sig;
-            lista->cursor[p].sig = lista->libre;
+            lista->inicio = lista->cursor[actual].siguiente;
+            lista->cursor[p].siguiente = lista->libre;
             lista->libre = p;
 	    borre = true;
         } else {
             for (int i = 0; i < pos - 2; i++) {
-                actual = lista->cursor[actual].sig;
+                actual = lista->cursor[actual].siguiente;
             }
             // actual apunta al nodo en posición (pos - 1)
-            p = lista->cursor[actual].sig; // nodo en pos
-            lista->cursor[actual].sig = lista->cursor[p].sig; // nodo en pos + 1
-            lista->cursor[p].sig = lista->libre;
+            p = lista->cursor[actual].siguiente; // nodo en pos
+            lista->cursor[actual].siguiente = lista->cursor[p].siguiente; // nodo en pos + 1
+            lista->cursor[p].siguiente = lista->libre;
             lista->libre = p;
             borre = true;
         }
@@ -202,19 +231,20 @@ partir de una clave y borra todas las ocurrencias.*/
 TipoElemento l_recuperar (Lista lista, int pos){
     int temp2 = lista->inicio;
     for (int i = 0; i < pos - 1; i++) {
-        temp2 = lista->cursor[temp2].sig;
+        temp2 = lista->cursor[temp2].siguiente;
     }
-    return lista->cursor[temp2].Dato;
+    return lista->cursor[temp2].datos;
 }
 //retorna el elemento (dato) de la posición “pos” recibida como parámetro.
 
 void l_mostrar (Lista lista){
-    int q=lista->inicio;
-    printf("mostrar el contenido de la lista:\n");
-    while(lista->cursor[q].sig==NULO){
-        prinf("%i\n",lista->cursor[q].Dato->clave);
-        q=lista->cursor[q].sig;
+    int temp2 = lista->inicio;
+    printf("Contenido de la lista: ");
+    while (temp2 != NULO) {
+        printf("%d ", lista->cursor[temp2].datos->clave);
+        temp2 = lista->cursor[temp2].siguiente;
     }
+    printf("\n");
 }
 //Muestra las claves de lista en la pantalla.
 
@@ -224,18 +254,18 @@ void l_mostrar (Lista lista){
 Iterador iterador(Lista lista) {
     Iterador iter = (Iterador) malloc(sizeof(struct IteradorRep));
     iter->lista = lista;
-    iter->pos_actual = lista->inicio;
+    iter->posicionActual = lista->inicio;
     return iter;
 }
 
 
 bool hay_siguiente(Iterador iterador) {
-    return (iterador->pos_actual != NULO);
+    return (iterador->posicionActual != NULO);
 }
 
 
 TipoElemento siguiente(Iterador iterador) {
-    TipoElemento actual = iterador->lista->cursor[iterador->pos_actual].Dato;
-    iterador->pos_actual = iterador->lista->cursor[iterador->pos_actual].sig;
+    TipoElemento actual = iterador->lista->cursor[iterador->posicionActual].datos;
+    iterador->posicionActual = iterador->lista->cursor[iterador->posicionActual].siguiente;
     return actual;
 }
